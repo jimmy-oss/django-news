6b5a0c3d50db55a3ad98cd176ce03a43e67b8ab8
jimmy-oss <jimmynjonge51@gmail.com>
2022-03-11 18:09:21 +0300

    updates

--- news/models.py
+++ news/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+ 
 class tags(models.Model):
     name = models.CharField(max_length =30)
 
@@ -9,13 +9,14 @@ class tags(models.Model):
 class Article(models.Model):
     title = models.CharField(max_length =60)
     post = models.TextField()
-    #editor = models.ForeignKey(Editor)
+   # editor = models.ForeignKey(Editor)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)
 class Editor(models.Model):
     first_name = models.CharField(max_length =30)
     last_name = models.CharField(max_length =30)
     email = models.EmailField()
+    phone_number = models.CharField(max_length = 10,blank =True)
 
     def __str__(self):
         return self.first_name
